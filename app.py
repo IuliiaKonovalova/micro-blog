8d6a4915896a722a95c285bd65eb1d3382a39ec9
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-04 21:51:59 +0800

    Connect home page with Flask app

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello World!"
+def home():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
