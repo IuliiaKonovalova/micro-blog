8e1672063d25197496333a6521af5bd87310d328
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-05 15:42:36 +0800

    Connect app with MongoDB

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ from flask import Flask, render_template, request
 if os.path.exists("env.py"):
     import env
 
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 entries = []
 
@@ -15,14 +24,14 @@ def home():
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
 
-        entries_with_date = [
-          (
-              entry[0],
-              entry[1],
-              datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
-          )
-          for entry in entries
-        ]
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
     return render_template("home.html", entries=entries_with_date)
 
 
