566a7b0b2d7bffb407a8293f0b7861af8bbf19d4
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-05 12:08:00 +0800

    Add datetime import and format the date

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template, request
 if os.path.exists("env.py"):
     import env
@@ -9,7 +10,8 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
-        print(entry_content)
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        print(entry_content, formatted_date)
     return render_template("home.html")
 
 
